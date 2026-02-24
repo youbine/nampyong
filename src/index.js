702bc0030da37345e0b1a403dfdf0c97794042cf
youbin <user@MacBook-Pro.local>
2026-02-24 14:48:14 +0900

    error1

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
